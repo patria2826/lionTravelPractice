9943ad87c2c32e4795aefeae556cec6eb32b31c3
Patria <patria2826@gmail.com>
2019-08-29 10:34:32 +0800

    module export/import practice

---
+++ js_module_export_2.js
@@ -0,0 +1,10 @@
+import { cube, foo, graph } from 'js_module_export_1.js';
+
+graph.options = {
+    color: 'blue',
+    thickness: '3px',
+}
+
+graph.draw();
+console.log(cub(3));
+console.log(foo);
