8dae3cb52ae436256fc68789c9e5ae9840edf4c9
Patria <patria2826@gmail.com>
2019-09-04 17:50:16 +0800

    seasons

---
+++ seasons/src/SeasonDisplay.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class SeasonDisplay extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+export default SeasonDisplay;
---
+++ seasons/src/index.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { lat: null, errMsg: '' };
+
+        window.navigator.geolocation.getCurrentPosition(
+            // callback
+            (position) => {
+                this.setState({ lat: position.coords.latitude });
+            },
+            // failure callback
+            (err) => {
+                this.setState({ errMsg: err.message });
+            }
+        );
+    }
+    render() {
+        let msg;
+        if (this.state.lat && !this.state.errMsg) {
+            msg = this.state.lat;
+        } else if (!this.state.lat && this.state.errMsg) {
+            msg = this.state.errMsg;
+        } else { msg = "Loading"; }
+        return (
+            <div>Latitude:  {msg}</div>
+        )
+    }
+}
+ReactDOM.render(
+    <App />,
+    document.querySelector("#root")
+)
