ce74cfc78f5336c7ebeed64218a1351e325e3856
Patria <patria2826@gmail.com>
2019-09-05 12:08:16 +0800

    done

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,12 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
+import './SeasonDisplay.css';
 
-export default class SeasonDisplay extends Component {
-    render() {
-        return (
-            <div>
-
-            </div>
-        )
+const seasonConfig = {
+    summer: {
+        text: 'Let\'s hit the beach!',
+        iconName: 'sun'
+    },
+    winter: {
+        text: 'Bur, it is chilly!',
+        iconName: 'snowflake'
     }
+}
+
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat > 0 ? 'winter' : 'summer';
+
+    }
+}
+
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    // destructure
+    const { text, iconName } = seasonConfig[season];
+    return (
+        <div className={`season-display ${season}`}>
+            <i className={`icon-left massive icon ${iconName}`}></i>
+            <h1>{text}</h1>
+            <i className={`icon-right massive icon ${iconName}`}></i>
+        </div>
+    )
+
 }
 export default SeasonDisplay;
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,42 +1,39 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = { lat: null, errMsg: '' };
-
+    state = { lat: null, errMsg: '' };  //等於在concstruction中定義this.state
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
             // callback
-            (position) => {
-                this.setState({ lat: position.coords.latitude });
-            },
+            (position) => { this.setState({ lat: position.coords.latitude }); },
             // failure callback
-            (err) => {
-                this.setState({ errMsg: err.message });
-            }
+            (err) => { this.setState({ errMsg: err.message }); }
         );
-    }
-    componentDidMount(){
         console.log('componentDidMount');
     }
-    componentDidUpdate(){
+    componentDidUpdate() {
         console.log('componentDidUpdate');
+    }
+    renderContent() {
+        //為了提升可維護性，盡量不要在render中進行條件判斷，所以把條件判斷的部分包到另一個function中
+        if (this.state.lat && !this.state.errMsg) {
+            return <SeasonDisplay lat={this.state.lat} />
+        } else if (!this.state.lat && this.state.errMsg) {
+        }
+        return <Spinner msg="Please accept location request" />
+        // return <Spinner />
     }
     render() {
         console.log('render');
-        let msg;
-        if (this.state.lat && !this.state.errMsg) {
-            msg = this.state.lat;
-        } else if (!this.state.lat && this.state.errMsg) {
-            msg = this.state.errMsg;
-        } else { msg = "Loading"; }
         return (
-            <div>Latitude:  {msg}</div>
+            <div>{this.renderContent()}</div>
         )
     }
 }
+
 ReactDOM.render(
     <App />,
     document.querySelector("#root")
