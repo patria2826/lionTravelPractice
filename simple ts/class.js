900ff343bcd99d106a108ae7615f337680b47904
Patria <patria2826@gmail.com>
2019-09-24 11:30:42 +0800

    class

--- simple ts/class.js
+++ simple ts/class.js
@@ -1,12 +1,66 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Player = /** @class */ (function () {
-    function Player(name, x, y) {
+    function Player(
+    // private readonly name: string,
+    // public name: string, 
+    name, //the child class can access to "protected" fields of the parent class
+    //when a field is passed into the constructor as an argument with prefix public/protected/private, it gets directly assigned from that argument value
+    // optional arguments must be declared after required argements
+    x, y) {
         this.name = name;
+        this.luckPointBase = Math.round(Math.random() * 10); //private is only enforced in ts at compilation time. Nothing in js will keep the field or method private
+        this.HP = 100;
+        this.MP = 100;
+        // when initializing sth inside constructor, there must be a type on the field declaration
+        Player.playerCnt++; //cannot use "this.playCnt"
         x ? this.x = x : this.x = 0;
         y ? this.y = y : this.y = 0;
     }
+    Player.prototype.display = function (prefix) {
+        console.log(prefix + (" " + this.name + " is at (" + this.x + "," + this.y + "). HP: " + this.HP + "%   MP: " + this.MP + "%"));
+    };
+    Player.prototype.updatePlayer = function () {
+    };
+    Player.playerCnt = 0; //"static" define properties on the class itself rather than the instances
+    Player.maxSpeed = 5; //readonly is only enforced in ts at compilation time. Nothing in js will pervent a readonly field being modified
     return Player;
 }());
-var p1 = new Player("Sam", 3, 5);
-var p2 = new Player("Leo");
-console.log(p1);
-console.log(p2);
+var Gunner = /** @class */ (function (_super) {
+    __extends(Gunner, _super);
+    function Gunner(//arguments declared here have nothing to do with the parent class
+    bulletsUsed, name, gunType, x, y) {
+        var _this = _super.call(this, name, x, y) || this;
+        _this.gunType = gunType;
+        _this.bulletsLeft = 10;
+        bulletsUsed > 10 ? _this.bulletsLeft = 0 : _this.bulletsLeft -= bulletsUsed;
+        return _this;
+    }
+    Gunner.prototype.updatePlayer = function () {
+        _super.prototype.updatePlayer.call(this); //inherits method of parent
+        this.bulletsLeft++;
+    };
+    Gunner.prototype.calMotion = function () { }; //method from parent
+    return Gunner;
+}(Player));
+var p1 = new Gunner(5, "Sam", "N/a"); //cannot new an abstract class
+var p2 = new Gunner(3, "Leo", "Nuclear Bomber", 3, 5);
+var p3 = new Gunner(2, "Jesicca", "Razor Gun", 2, 3);
+var p4 = new Gunner(10, "Nancy", "Machine Gun", 1, 1);
+p1.display('Rooky Player');
+p2.display('Pro Player');
+p3.display('Pro Player');
+p4.display('Rooky Player');
+var objs = [p1, p2, p3, p4];
+console.log(objs);
--- simple ts/class.ts
+++ simple ts/class.ts
@@ -1,16 +1,79 @@
-class Player {
-    name: string;
+interface GameObj {
+    HP: number,
+    MP: number,
+    updatePlayer(): void
+}
+
+abstract class Player implements GameObj {
+    static playerCnt: number = 0; //"static" define properties on the class itself rather than the instances
+    static readonly maxSpeed: number = 5; //readonly is only enforced in ts at compilation time. Nothing in js will pervent a readonly field being modified
+    private luckPointBase: number = Math.round(Math.random() * 10); //private is only enforced in ts at compilation time. Nothing in js will keep the field or method private
+
+
     x: number;
     y: number;
-    constructor(name: string, x?: number, y?: number) {
-        this.name = name;
+    HP = 100;
+    MP = 100;
+    constructor(
+        // private readonly name: string,
+        // public name: string, 
+        protected readonly name: string,  //the child class can access to "protected" fields of the parent class
+        //when a field is passed into the constructor as an argument with prefix public/protected/private, it gets directly assigned from that argument value
+
+        // optional arguments must be declared after required argements
+        x?: number,
+        y?: number) {
+        // when initializing sth inside constructor, there must be a type on the field declaration
+        Player.playerCnt++; //cannot use "this.playCnt"
+
         x ? this.x = x : this.x = 0;
         y ? this.y = y : this.y = 0;
     }
+
+    display(prefix: string): void {
+        console.log(
+            prefix + ` ${this.name} is at (${this.x},${this.y}). HP: ${this.HP}%   MP: ${this.MP}%`
+        )
+    }
+
+    updatePlayer() {
+
+    }
+
+    abstract calMotion(): void; //cannot have an implementation because it is marked abstract
 }
 
-let p1 = new Player("Sam", 3, 5);
-let p2 = new Player("Leo");
+class Gunner extends Player {
+    bulletsLeft: number = 10;
 
-console.log(p1);
-console.log(p2);
+    constructor( //arguments declared here have nothing to do with the parent class
+        bulletsUsed: number,
+        name: string,
+        public gunType: string,
+        x?: number,
+        y?: number,
+    ) {
+        super(name, x, y); //inherits arguments declared in the parent class when using super
+        bulletsUsed > 10 ? this.bulletsLeft = 0 : this.bulletsLeft -= bulletsUsed;
+    }
+    
+    updatePlayer() { //has nothig to do with method of parent
+        super.updatePlayer(); //inherits method of parent
+        this.bulletsLeft++;
+    }
+
+    calMotion() { } //method from parent
+}
+
+let p1 = new Gunner(5, "Sam", "N/a"); //cannot new an abstract class
+let p2 = new Gunner(3, "Leo", "Nuclear Bomber", 3, 5);
+let p3 = new Gunner(2, "Jesicca", "Razor Gun", 2, 3);
+let p4 = new Gunner(10, "Nancy", "Machine Gun", 1, 1);
+
+p1.display('Rooky Player');
+p2.display('Pro Player');
+p3.display('Pro Player');
+p4.display('Rooky Player');
+
+let objs: GameObj[] = [p1, p2, p3, p4];
+console.log(objs);
