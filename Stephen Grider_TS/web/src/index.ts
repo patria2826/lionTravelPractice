bec527a014195ffe93ea5ebce18e1658e8f1dea1
Patria <patria2826@gmail.com>
2019-10-05 15:35:50 +0800

    show all users

--- Stephen Grider_TS/web/src/index.ts
+++ Stephen Grider_TS/web/src/index.ts
@@ -1,16 +1,25 @@
 import { UserEdit } from "./views/UserEdit";
-import { User } from "./models/User";
+import { User, UserProps } from "./models/User";
+import { UserList } from "./views/UserList";
+import { Collection } from "./models/Collection";
 
 const user = User.buildUser({ name: "jake", age: 6 });
+const users = new Collection(
+  "http://localhost:3000/users",
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
 
 const root = document.getElementById("root");
 
 if (root) {
-  const userEdit = new UserEdit(root, user);
+  new UserEdit(root, user).render();
 
-  userEdit.render();
-
-  console.log(userEdit);
+  users.fetch();
+  users.on("change", () => {
+    new UserList(root, users).render();
+  });
 } else {
   throw new Error("User Element Not Found.");
 }
