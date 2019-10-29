dfaf809f355270f70ddeb62e102c30e128b71ff7
Patria <patria2826@gmail.com>
2019-10-29 16:19:44 +0800

    translate done

--- translate/src/components/Button.tsx
+++ translate/src/components/Button.tsx
@@ -3,25 +3,28 @@ import LanguageContext from "../store/LanguageContext";
 import ColorContext from "../store/ColorContext";
 
 const Button = (props: any) => {
-  // helper function
+  // helper functions
   const submitBtnText = (value: string): ReactNode => {
     return value === "english" ? "Submit" : "送出";
   };
+
+  const renderBtn = (color: string): ReactNode => {
+    return (
+      <button className={`ui button ${color}`}>
+        <LanguageContext.Consumer>
+          {(value: string): ReactNode => {
+            return submitBtnText(value);
+          }}
+        </LanguageContext.Consumer>
+      </button>
+    );
+  };
   return (
+    //  the child of consumer must be a funciton
+    //  use the consumer when retrieving multiple context objs
     <ColorContext.Consumer>
       {(color: string): ReactNode => {
-        return (
-          <button className={`ui button ${color}`}>
-            {/* the child of consumer must be a funciton */}
-            {/* use the consumer when retrieving multiple context objs */}
-            {/* not necessary while using hooks? */}
-            <LanguageContext.Consumer>
-              {(value: string): ReactNode => {
-                return submitBtnText(value);
-              }}
-            </LanguageContext.Consumer>
-          </button>
-        );
+        return renderBtn(color);
       }}
     </ColorContext.Consumer>
   );
