393e12bab33394fdbfe15388ef8ca25ee920cc5a
Patria <patria2826@gmail.com>
2020-02-20 18:05:37 +0800

    大改

--- Colt Steele_React/pokedex/src/PokeGame.tsx
+++ Colt Steele_React/pokedex/src/PokeGame.tsx
@@ -1,165 +1,40 @@
 import React, { useState, useEffect } from "react";
 import Pokecard from "./components/Pokecard";
-import axios from "axios";
+// import axios from "axios";
 import "./PokeGame.css";
 
-interface pokemonNameAndId {
-  name: string;
-  id: number;
-}
-
-function PK(team1: number, team2: number): boolean {
-  return team1 - team2 > 0;
-}
-
 function PokeGame(props: any) {
+  const [firstTeam, setFirstTeam] = useState<number[]>([]);
+  const [secondTeam, setSecondTeam] = useState<number[]>([]);
   const pokeLimit = 806;
   const memberCnt = 6;
-  const POKE_API = `https://pokeapi.co/api/v2/pokemon/`;
-  const [battlePoints, setBattlePoints] = useState<{ [id: string]: number }[]>(
-    []
-  );
-  const [startPK, setStartPK] = useState<boolean>(false);
-
-  const [team1, setTeam1] = useState<pokemonNameAndId[]>([]);
-  const [team2, setTeam2] = useState<pokemonNameAndId[]>([]);
-
-  const getTeam = (
-    setTeam: (f: (pokemon: pokemonNameAndId[]) => pokemonNameAndId[]) => void
-  ) => {
-    for (let i = 0; i < memberCnt; i++) {
-      axios
-        .get(`${POKE_API}${Math.floor(Math.random() * pokeLimit) + 1}`)
-        .then(response => {
-          setTeam((prev: pokemonNameAndId[]) => {
-            //to do the rest things, prev(original value of team1) must not be empty
-            return [
-              ...prev,
-              { name: response.data.name, id: response.data.id }
-            ];
-          });
-        });
-    }
-  };
+  //   const imgApi = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
+  //   const dataApi = `https://pokeapi.co/api/v2/pokemon/`;
 
   useEffect(() => {
-    getTeam(setTeam1);
-    getTeam(setTeam2);
+    setFirstTeam(getTeam());
+    setSecondTeam(getTeam());
   }, []);
 
+  const getTeam = (): number[] => {
+    const team: number[] = [];
+    for (let i = 0; i < memberCnt; i++) {
+      team.push(Math.ceil(Math.random() * pokeLimit));
+    }
+    return team;
+  };
+
+  const renderTeam = (team: number[]) => {
+    return team.map((id: number, key: number) => {
+      return <Pokecard key={key} id={id}></Pokecard>;
+    });
+  };
+
   return (
-    <div className="PokeGame">
-      {team1.length > 0 && team2.length > 0 && (
-        <div>
-          <div className="PokeGame-team">
-            <h2>Team 1</h2>
-            <div className="PokeGame-members">
-              {team1.map((pokemon, i) => {
-                return (
-                  <div key={i} className="PokeGame-each-member">
-                    <Pokecard
-                      pokeName={pokemon["name"]}
-                      id={pokemon["id"]}
-                      getBP={(battlePoint: number) => {
-                        setBattlePoints(prev => {
-                          return [
-                            ...prev,
-                            { [pokemon["id"].toString()]: battlePoint }
-                          ];
-                        });
-                      }}
-                    ></Pokecard>
-                    {((): any => {
-                      if (startPK) {
-                        const oppoent = team2.map(pokemon => {
-                          return pokemon["id"].toString();
-                        })[i];
-                        const idObj = battlePoints.filter(element => {
-                          const key = Object.keys(element)[0];
-                          if (
-                            key === pokemon["id"].toString() ||
-                            key === oppoent
-                          ) {
-                            return element[key];
-                          }
-                        });
-                        if (idObj.length === 2) {
-                          if (idObj[0][pokemon["id"]] && idObj[1][oppoent]) {
-                            return (
-                              idObj[0][pokemon["id"]] > idObj[1][oppoent] && (
-                                <div className="PokeGame-winner">Winner!!</div>
-                              )
-                            );
-                          }
-                        }
-                      }
-                    })()}
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-          {battlePoints[memberCnt * 2 - 1] && (
-            <button
-              onClick={() => {
-                setStartPK(true);
-              }}
-            >
-              Battle
-            </button>
-          )}
-          <div className="PokeGame-team">
-            <h2>Team 2</h2>
-            <div className="PokeGame-members">
-              {team2.map((pokemon, i) => {
-                return (
-                  <div key={i} className="PokeGame-each-member">
-                    <Pokecard
-                      pokeName={pokemon["name"]}
-                      id={pokemon["id"]}
-                      getBP={(battlePoint: number) => {
-                        setBattlePoints(prev => {
-                          return [...prev, { [pokemon["id"]]: battlePoint }];
-                        });
-                      }}
-                    ></Pokecard>
-                    {((): any => {
-                      if (startPK) {
-                        const oppoent = team1.map(pokemon => {
-                          return pokemon["id"].toString();
-                        })[i];
-                        const idObj = battlePoints.filter(element => {
-                          const key = Object.keys(element)[0];
-                          if (
-                            key === pokemon["id"].toString() ||
-                            key === oppoent
-                          ) {
-                            return element[key];
-                          }
-                        });
-                        if (idObj.length === 2) {
-                          console.log(idObj);
-                          console.log(
-                            `${idObj[0][oppoent]} vs ${idObj[1][pokemon["id"]]}`
-                          );
-                          if (idObj[0][oppoent] && idObj[1][pokemon["id"]]) {
-                            return (
-                              idObj[0][oppoent] < idObj[1][pokemon["id"]] && (
-                                <div className="PokeGame-winner">Winner!!</div>
-                              )
-                            );
-                          }
-                        }
-                      }
-                    })()}
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-        </div>
-      )}
-    </div>
+    <>
+      <div className="T1">{firstTeam[0] && renderTeam(firstTeam)}</div>
+      <div className="T2">{secondTeam[0] && renderTeam(firstTeam)}</div>
+    </>
   );
 }
 export default PokeGame;
--- Colt Steele_React/pokedex/src/components/Pokecard.tsx
+++ Colt Steele_React/pokedex/src/components/Pokecard.tsx
@@ -2,92 +2,90 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./Pokecard.css";
 
-interface pokemonStat {
-  [key: string]: number;
+export interface pokemonData {
+  name: string;
+  id: number;
+  type: string[];
+  state: {
+    [key: string]: number;
+  };
 }
 
 function firstCharUpperCase(string: string): string {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-function Pokecard(props: any) {
-  const { pokeName, id, getBP } = props;
+const Pokecard = (props: any) => {
+  const [pokemonData, setPokemonData] = useState<pokemonData>();
+  const { id } = props;
   const imgApi = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
   const dataApi = "https://pokeapi.co/api/v2/pokemon/";
   let battlePoint = 0;
 
-  const [pokeTypes, setPokeTypes] = useState<string[]>([]);
-  const [pokeStat, setPokeStat] = useState<pokemonStat[]>([]);
-
   useEffect(() => {
     axios.get(`${dataApi}${id}/`).then(response => {
-      response.data.types.map((type: { type: { name: string } }) => {
-        setPokeTypes(prev => {
-          return [...prev, type.type.name];
-        });
+      console.log(response.data);
+      const types = [];
+      setPokemonData({
+        name: response.data.name,
+        id: response.data.id,
+        type: [],
+        state: response.data.name
       });
-      response.data.stats.map(
-        (status: { base_stat: number; stat: { name: string } }) => {
-          setPokeStat(prev => {
-            return [...prev, { [status.stat.name]: status.base_stat }];
-          });
-        }
-      );
     });
   }, []);
 
-  useEffect(() => {
-    if (getBP) {
-      if (pokeStat.length >= 6) {
-        let temp = 0;
-        pokeStat.map((status: { [key: string]: number }) => {
-          const key = Object.keys(status)[0];
-          temp += status[key];
-        });
-        getBP(temp);
-      }
-    }
-  }, [pokeStat]);
+  //   useEffect(() => {
+  //     if (getBP) {
+  //       if (pokeStat.length >= 6) {
+  //         let temp = 0;
+  //         pokeStat.map((status: { [key: string]: number }) => {
+  //           const key = Object.keys(status)[0];
+  //           temp += status[key];
+  //         });
+  //         getBP(temp);
+  //       }
+  //     }
+  //   }, [pokeStat]);
 
   return (
-    <div className="Pokecard">
-      <h3 className="Pokecard-name">{firstCharUpperCase(pokeName)}</h3>
-      <img
-        src={`${imgApi}${id < 100 ? (id < 10 ? "00" : "0") : ""}${id}.png`}
-        alt={pokeName}
-        className="Pokecard-img"
-      />
-      <p className="Pokecard-type-title">
-        {pokeTypes.map((type, i) => {
-          return (
-            <span key={i} className={`Pokecard-types ${type}`}>
-              {type}
-            </span>
-          );
-        })}
-      </p>
-      <div className="Pokecard-stat">
-        {pokeStat.map((status: { [key: string]: number }, i) => {
-          const key = Object.keys(status)[0];
-          battlePoint += status[key];
-          return (
-            <span key={i} className={`Pokkecard-stat-each ${key}`}>
-              <p className="Pokecard-stat-title">{firstCharUpperCase(key)}</p>
-              <span
-                className="Pokecard-stat-bar"
-                style={{ width: `${status[key] * 0.6}px` }}
-              ></span>
-              <span className="Pokecard-stat-val">{status[key]}</span>
-            </span>
-          );
-        })}
-        <h4 className="Pokecard-BP">
-          Total Battle Point:{" "}
-          <span className="Pokecard-total">{battlePoint}</span>
-        </h4>
-      </div>
-    </div>
+    <div>{id}</div>
+    //   <h3 className="Pokecard-name">{firstCharUpperCase(pokeName)}</h3>
+    //   <img
+    //     src={`${imgApi}${id < 100 ? (id < 10 ? "00" : "0") : ""}${id}.png`}
+    //     alt={pokeName}
+    //     className="Pokecard-img"
+    //   />
+    //   <p className="Pokecard-type-title">
+    //     {pokeTypes.map((type, i) => {
+    //       return (
+    //         <span key={i} className={`Pokecard-types ${type}`}>
+    //           {type}
+    //         </span>
+    //       );
+    //     })}
+    //   </p>
+    //   <div className="Pokecard-stat">
+    //     {pokeStat.map((status: { [key: string]: number }, i) => {
+    //       const key = Object.keys(status)[0];
+    //       battlePoint += status[key];
+    //       return (
+    //         <span key={i} className={`Pokkecard-stat-each ${key}`}>
+    //           <p className="Pokecard-stat-title">{firstCharUpperCase(key)}</p>
+    //           <span
+    //             className="Pokecard-stat-bar"
+    //             style={{ width: `${status[key] * 0.6}px` }}
+    //           ></span>
+    //           <span className="Pokecard-stat-val">{status[key]}</span>
+    //         </span>
+    //       );
+    //     })}
+    //     <h4 className="Pokecard-BP">
+    //       Total Battle Point:{" "}
+    //       <span className="Pokecard-total">{battlePoint}</span>
+    //     </h4>
+    //   </div>
   );
-}
+};
 
 export default Pokecard;
